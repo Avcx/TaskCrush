bf68d1191a8a7655b41545faa741f77cf5f857a9
Avcx <avcx12@gmail.com>
2017-03-25 15:59:12 -0600

    Added more and improved animations

--- App/scripts/register.js
+++ App/scripts/register.js
@@ -1,10 +1,13 @@
 if ("serviceWorker" in navigator) {
   window.onload = () => {
-    navigator.serviceWorker.register("../sw-cache.js").then((scope) => {
-      console.log(scope.scope);
-    }
-    ).catch(err => console.log(err))
+    navigator.serviceWorker.register("../sw-cache.js")
+    .then(scope => console.log(scope.scope))
+    .catch(err => console.log(err))
   };
 }
 
-var request = window.indexedDB.open("TodoList", 1);
+let request = window.indexedDB.open("TodoList", 1);
+
+request.onload = (event) => {
+  console.log(event);
+}
