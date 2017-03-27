3e1f6efa171eae762e23d6f369c68bc2f20c6805
Avcx <avcx12@gmail.com>
2017-03-26 19:44:09 -0600

    App Shell fully works offline

--- App/sw-cache.js
+++ App/sw-cache.js
@@ -7,21 +7,22 @@ let CACHE = [
   "/data/manifest.json",
   "/styles/animations.css",
   "/scripts/register.js",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
   "sw-cache.js"
 ];
 
-let CACHE_VERSION = ["7"];
+let CACHE_VERSION = ["4"];
 
 let install = function(event) {
   event.waitUntil(
-    caches.open(CACHE_VERSION).then(function(cache) { cache.addAll(CACHE)}))
+    caches.open(CACHE_VERSION).then(cache => cache.addAll(CACHE)).then(
   caches.keys().then(function(keyList) {
     return Promise.all(keyList.map(function(key) {
-      if (CACHE_VERSION.indexOf(key) === -1) {
-        return caches.delete(key);
-      }
+      if (CACHE_VERSION.indexOf(key) === -1) return caches.delete(key);
     }));
-  })
+    })
+  )
+)
   console.log("installed.")
 }
 
